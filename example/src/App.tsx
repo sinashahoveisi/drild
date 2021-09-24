f41042b1f333344de3d262b2bf8bb64ca1028f89
sinashahoveisi <shahoveisysina@yahoo.com>
2021-09-24 21:36:23 +0330

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'drilld'
+import 'drilld/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
